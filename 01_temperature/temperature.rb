bf3e66160c713384b939f9008c427550b66fed06
Kendrick Hilton <k21confirm@gmail.com>
2019-07-30 19:32:59 -0400

    Completed 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+
+def ftoc(number)
+    number = (number.to_f - 32) * 5/9
+    return number
+end
+
+def ctof(number)
+    number = (number.to_f * 9/5) + 32
+end
--- 01_temperature/temperature_spec.rb
+++ 01_temperature/temperature_spec.rb
@@ -21,7 +21,7 @@ describe "temperature conversion functions" do
 
   describe "#ftoc" do
 
-    it "converts freezing temperature" do
+    it "converts freezing temperature" do 
       expect(ftoc(32)).to eq(0)
     end
 
