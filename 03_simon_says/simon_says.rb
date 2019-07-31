c18d0824d620468014ceaff83457ab9f8be5b9d6
Kendrick Hilton <k21confirm@gmail.com>
2019-07-31 17:11:52 -0400

    Finished 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,49 @@
-#write your code here
+ def echo(string)
+    return string
+ end
+
+ def shout(string)
+    return string.upcase
+ end
+
+ def repeat(string, num = 2)
+    counter = 0
+    array = []
+    while counter < num
+        array[counter] = string
+        counter += 1
+    end 
+    return array.join(' ')
+ end
+
+
+ def start_of_word(word, num = 1)
+    return word[0...num]
+ end
+
+ def first_word(string)
+    return string.split(' ')[0]
+ end
+
+ def titleize(string)
+    words = string.split(' ')
+
+    index = 0;
+
+    while index < words.length
+        if words[index] == 'and' || words[index] == 'the' || words[index] == 'over'
+            index += 1
+        else 
+            word = words[index]
+            word[0] = word[0].capitalize
+            index += 1
+        end
+    end
+
+    words[0] = words[0].capitalize
+
+    
+    return words.join(' ') 
+end
+
+ titleize("joe and mac")
