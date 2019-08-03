a7c0ec07e653c561743074832abfc3b9c9099b9e
Kendrick Hilton <k21confirm@gmail.com>
2019-08-03 13:46:01 -0400

    finished 05_gook_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+    attr_reader :title
+
+ def title= (new_title)
+    words = new_title.split(' ')
+
+    index = 0;
+    small_words = ['a', 'and', 'the', 'of', 'over', 'an', 'in']
+
+    while index < words.length
+        if small_words.include? words[index]
+            index += 1
+        else 
+            word = words[index]
+            word[0] = word[0].capitalize
+            index += 1
+        end
+    end
+
+    words[0] = words[0].capitalize
+
+    
+    @title = words.join(' ')
+ end
 end
