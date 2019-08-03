393982339667881ce74daf7ce029e05903c7b92c
Kendrick Hilton <k21confirm@gmail.com>
2019-08-03 15:01:59 -0400

    sompleted 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
-  #write your code here
+  attr_accessor(:seconds)
+
+  def seconds
+    @seconds = 0
+  end
+
+  def time_string
+    second = @seconds
+    time = [0, 0, 0]
+    time[0] = pad(second / 3600)
+    second = second % 3600
+    time[1] = pad(second / 60)
+    second = second % 60
+    time[2] = pad(second)
+
+    return time.join(':') 
+  end
+
+  def pad(num)
+    if num < 10
+      return ('0' + num.to_s)
+    else 
+      return num.to_s
+    end
+  end
 end
